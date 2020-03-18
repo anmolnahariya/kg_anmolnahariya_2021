95e015b8af2ca996e59f3699c81548ec0ac937c4
Anmol Nahariya <anmolnahariya@Anmols-MBP.fios-router.home>
2020-03-17 21:22:48 -0400

    Commit 2
    Loop created to process all characters one by one. If a character in second string is seen again, mapping is not possible. Mark visited characters and store mapping of all characters. If character in first string occurs more than once, check previous mapping of that character.

--- main.py
+++ main.py
@@ -9,3 +9,20 @@ def str_map(a,b):
 
     marked = [False] * 256 #256 ASCII codes
     char_map = [-1] * 256
+
+    for i in range(len(a)):
+        if char_map[ord(a[i])] == -1:
+            if marked[ord(a[i])] == True:
+                print(False)
+                exit(0)
+
+            marked[(ord(b[i]))] = True
+            char_map[ord(a[i])] = b[i]
+
+        elif char_map[ord(a[i])] != b[i]:
+            print(False)
+            exit(0)
+    print(True)
+    exit(0)
+
+str_map(s1,s2)
